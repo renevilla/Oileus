ae240147fd70b2ae3e92b996e3bd17ec4d96bf01
Rene Villa <renevilla23@gmail.com>
2015-02-01 15:00:36 -0800

    working edits

--- json-jquery.js
+++ json-jquery.js
@@ -1,10 +1,32 @@
-$(document).ready(function(){ 
-	//attach a jQuery live event to the button
-	$('#getdata-button').live('click', function(){
-		$.getJSON('json-data.php', function(data) {	
-			//alert(data); //uncomment this for debug
-			//alert (data.item1+" "+data.item2+" "+data.item3); //further debug
-			$('#showdata').html("<p>item1="+data.item1+" item2="+data.item2+" item3="+data.item3+"</p>");
-		});
-	});
-});
+
+			$(document).ready(function() {
+
+				//load data after button is clicked
+				$(".button").on("click", function() {
+
+
+					//ajax magic
+					$.ajax({
+						type: "GET",
+						url: "http://www.reneinla.com/data.json",
+						dataType: "json",
+						xhrFields: {
+							withCredentials: false
+						},
+						headers:{
+
+						},
+						crossDomain: true, // enable this
+						success: function(data) {
+
+							//data downloaded so we call parseJSON function
+							//and pass downloaded data
+							var json = data;
+							//now json variable contains data in JSON format
+							//lets display a few items
+							$('#here').html("First Name: " + json.firstName + "<br />Last Name: " + json.lastName);
+						}
+					});
+				});
+			});
+		
