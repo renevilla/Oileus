d95cb50775a674230d4bb9a0ca3db5dd2383411f
Rene Villa <renevilla23@gmail.com>
2015-02-02 22:55:15 -0800

    new stuff

--- json-jquery.js
+++ json-jquery.js
@@ -1,32 +1,36 @@
+$(document).ready(function() {
 
-			$(document).ready(function() {
+	//load data after button is clicked
+	$(".button").on("click", function() {
 
-				//load data after button is clicked
-				$(".button").on("click", function() {
+		//setInterval(function() {
+			//ajax magic
+			$.ajax({
+				type: "GET",
+				url: "http://www.reneinla.com/data.json",
+				dataType: "json",
+				beforeSend: function() {
+					console.log('before ajax request starts!');
+				},
+				xhrFields: {
+					withCredentials: false
+				},
+				crossDomain: true, // enable this
+				success: function(data) {
 
-
-					//ajax magic
-					$.ajax({
-						type: "GET",
-						url: "http://www.reneinla.com/data.json",
-						dataType: "json",
-						xhrFields: {
-							withCredentials: false
-						},
-						headers:{
-
-						},
-						crossDomain: true, // enable this
-						success: function(data) {
-
-							//data downloaded so we call parseJSON function
-							//and pass downloaded data
-							var json = data;
-							//now json variable contains data in JSON format
-							//lets display a few items
-							$('#here').html("First Name: " + json.firstName + "<br />Last Name: " + json.lastName);
-						}
+					//data downloaded so we call parseJSON function
+					//and pass downloaded data
+					var json = data;
+					//now json variable contains data in JSON format
+					//lets display a few items
+					//$('#here').html("my Name: " + json.quote);
+					//alert(json.quote);
+					$.each(json, function(idx, obj) {
+						alert(obj.tagName);
 					});
-				});
+				}
 			});
+		//});
+	});
+});
 		
